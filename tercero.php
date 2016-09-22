bf62dea6c655d988ccc4a66af958183696e6ab85
Fabrizzio Pereira Vasquez <fabrizzio_vasquez@hotmail.com>
2016-09-21 21:29:34 -0500

    mi primer commit

---
+++ tercero.php
@@ -0,0 +1,22 @@
+<!DOCTYPE htlm>
+<html>
+<head> 
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Pokemon Go</title>
+</head>                             
+<body>
+     <div  class="col-md-4 center-block quitar-float text-center espacio-arriba">
+        <img src="imgs/pokemon4.jpg.jpg">
+        <h1>Juega el mejor juego de todo el mundo</h1>  
+        <h3>Este juego no lo vas a encontrar en otro lugar</h3>
+        <nav>
+          <a href="https://mail.google.com/mail/&hl=es-419">Menú</a>
+          <a href="https://twitter.com/?lang=es">Contacto</a>
+          <a href="https://www.facebook.com/">Acerca de</a>
+        </nav>
+    </div>
+</body>     
+</html>
+              
